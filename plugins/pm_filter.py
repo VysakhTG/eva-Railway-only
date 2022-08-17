bf9ae20871699002b9f7daeec9152cbe19ea3ee5
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-17 16:27:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -705,7 +705,6 @@ async def auto_filter(client, msg, spoll=False):
             plot=imdb['plot'],
             rating=imdb['rating'],
             url=imdb['url'], 
-            time=datetime.now(timezone('Asia/Kolkata')).strftime("%d/%m/%Y, %H:%M:%S"),
             **locals()
         )
     else:
