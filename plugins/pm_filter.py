b85e5778e3bfe72e7c041e6fac557731ce692a89
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-21 14:45:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -138,7 +138,7 @@ async def advantage_spoll_choker(bot, query):
             k = await query.message.edit('This Movie Not Found In DataBase')
             await asyncio.sleep(10)
             await k.delete()
-
+            await message.delete()
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
