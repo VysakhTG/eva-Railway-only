834e53d3a0c4755e7c70d2483c465cdbdb337695
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-25 21:37:31 +0530

    Create ping.py

---
+++ plugins/extra/ping.py
@@ -0,0 +1,9 @@
+from pyrogram import Client, filters 
+
+@Client.on_message(filters.private & filters.command(["ping"]))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
