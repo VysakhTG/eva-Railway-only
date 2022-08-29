69241eca63cef037984702ca4ba1fd0ca0d78930
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-29 17:29:09 +0530

    Create forward.py

---
+++ plugins/extra/forward.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters 
+from pyrogram.types import Message 
+import time 
+import os 
+from info import ADMINS 
+
+@Client.on_message(filters.private & filters.command(["forward"]))
+async def forward(client, message): 
+    msg = await bot.ask(m.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**") 
+    t_chat = msg.forward_from_chat.id 
+    msg1 = await bot.ask(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**") 
+    msg2 = await bot.ask(m.chat.id, "**Send Ending Message from same chat**") 
+    # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id) 
+    i_chat = msg1.forward_from_chat.id 
+    s_msg = int(msg1.forward_from_message_id)
+    f_msg = int(msg2.forward_from_message_id)+1 
+    await m.reply_text('**Forwarding Started**\n\nPress /restart to Stop and /log to get log TXT file') 
+    try:      
+        for i in range(s_msg, f_msg):
+            try:
+                await bot.copy_message(
+                    chat_id= t_chat,
+                    from_chat_id= i_chat,
+                    message_id= i
+                ) 
+                time.sleep(2)
+            except Exception: 
+                continue 
+    except Exception as e: 
+        await m.reply_text(str(e)) 
+    await m.reply_text("Done Forwarding")
