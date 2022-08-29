13ff3ff48210cda6ddcf732d2b7a87ebcda3c42f
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-29 18:20:02 +0530

    Update forward.py

--- plugins/extra/forward.py
+++ plugins/extra/forward.py
@@ -6,10 +6,10 @@ from info import ADMINS
 
 @Client.on_message(filters.private & filters.command(["forward"]))
 async def forward(bot: Client, m: Message): 
-    msg = await bot.ask(m.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
+    msg = await m.reply_text(m.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
     t_chat = msg.forward_from_chat.id 
-    msg1 = await bot.ask(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
-    msg2 = await bot.ask(m.chat.id, "**Send Ending Message from same chat**")
+    msg1 = await m.reply_text(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
+    msg2 = await m.reply_text(m.chat.id, "**Send Ending Message from same chat**")
     # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id) 
     i_chat = msg1.forward_from_chat.id 
     s_msg = int(msg1.forward_from_message_id)
