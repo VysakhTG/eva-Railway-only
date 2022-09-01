3d5ad1eec0019d77b26d552175037f5fbd3bc654
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-01 20:14:11 +0530

    Update bug.py

--- plugins/extra/bug.py
+++ plugins/extra/bug.py
@@ -3,3 +3,5 @@ from datetime import datetime
 from pyrogram import Client,filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message 
 
+@Client.on_message(filters.command("bug"))
+async def bug(bot, message):
