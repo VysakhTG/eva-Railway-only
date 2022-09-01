214035b838daf87f66fb83d15b66849a7398ad23
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-01 20:39:51 +0530

    Update bug.py

--- plugins/extra/bug.py
+++ plugins/extra/bug.py
@@ -13,12 +13,13 @@ async def bug(bot, message):
     user_id = msg.from_user.id
         if bugs:
             await msg.reply_text(
-                f"<b>ʙᴜɢ ʀᴇᴩᴏʀᴛ : {bugs}</b>\n\n"
+                f"<b>ʙᴜɢ ʀᴇᴩᴏʀᴛ : </b>\n\n"
                 "<b>» ʙᴜɢ sᴜᴄᴄᴇssғᴜʟʟʏ ʀᴇᴩᴏʀᴛᴇᴅ ᴀᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ !</b>",
                 reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data=f"close_reply")]]
                 ),
             )
+    await client.send_message(LOG_CHANNEL, script.LOG_TEXT_B.format(bugs, message.from_user.mention, message.from_user.id))
 @Client.on_callback_query(filters.regex("close_reply"))
 async def close_reply(msg, CallbackQuery):
     await CallbackQuery.message.delete()
