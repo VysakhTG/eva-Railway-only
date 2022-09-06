d1d7d6a5281243e32e2aab5773a743214ded5cb4
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-06 15:20:42 +0530

    Update delete.py

--- plugins/extra/delete.py
+++ plugins/extra/delete.py
@@ -10,4 +10,4 @@ async def adelete(bot, message):
           return
        else:
           await asyncio.sleep(5)
-          await bot.delete_messages(message.chat.id)
+          await bot.delete_messages
