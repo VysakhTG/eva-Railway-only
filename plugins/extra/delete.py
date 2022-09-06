5283ef385f897ed9468f508fee5e9aff4e884fdc
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-06 15:13:56 +0530

    Create delete.py

---
+++ plugins/extra/delete.py
@@ -0,0 +1,13 @@
+import asyncio
+from info import ADMINS
+from os import environ
+from pyrogram import Client, filters, idle
+
+@Client.on_message(filters.command(["adelete"])) 
+async def adelete(bot, message):
+    try:
+       if message.from_user.id not in ADMINS:
+          return
+       else:
+          await asyncio.sleep(5)
+          await Bot.delete_messages(message.chat.id, message.id)
