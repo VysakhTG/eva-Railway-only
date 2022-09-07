def1759b569f45705f14ec54388814f4eccd2997
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-07 15:46:15 +0530

    Update forward.py

--- plugins/extra/forward.py
+++ plugins/extra/forward.py
@@ -7,7 +7,7 @@ from info import ADMINS
 
 @Client.on_message(filters.private & filters.command(["forward"]))
 async def forward(client, message): 
-    msg = await client.ask(chat_id=query.message.chat.id, text="<b>❪ SET TARGET CHAT ❫\n\nForward a message from Your target chat</b>")
+    msg = await client.send_message(chat_id=query.message.chat.id, text="<b>❪ SET TARGET CHAT ❫\n\nForward a message from Your target chat</b>")
     t_chat = message.forward_from_chat
     msg1 = await bot.ask(chat_id=query.message.chat.id, text="<b>Send Starting Message From Where you want to Start forwarding</b>")
     msg2 = await bot.ask(chat_id=query.message.chat.id, text="<b>Send Ending Message from same cha</b>")
