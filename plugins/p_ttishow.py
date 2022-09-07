630bcec00f52630acaee81f3a76f6cc3d090c49d
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-07 15:26:40 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -55,7 +55,7 @@ async def save_group(bot, message):
                         pass
                 temp.MELCOW['welcome'] = await message.reply(f"<b>✨ Good Morning {u.mention}!\nWelcome to {message.chat.title}\n\n🔘 No Promo, No Porn, No Other Abuses\n🔘 Ask Your Movies With Correct Spelling\n🔘 Spammers Stay Away\n🔘 Feel Free To Report Any Errors To Admins using @admin\n\n🔰 Share & Support Us 🔰</b>")
 await asyncio.sleep(10)
-temp.MELCOW['welcome'].delete()
+await temp.MELCOW['welcome'].delete()
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
