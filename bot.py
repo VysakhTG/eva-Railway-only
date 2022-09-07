40426ac640ac0be644ddf5d9d38ae3aaa7a58807
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-07 15:34:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,6 +45,7 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         k = await self.send_message(LOG_CHANNEL, (f"🤖 Bot Restarted!"))
+        try:
             await k.pin()
         logging.info(LOG_STR)
 
