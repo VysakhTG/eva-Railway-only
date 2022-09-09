002f0fbeb6bf2584930d4cb75cc4e721fe7d9c21
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-09 16:25:13 +0530

    Update caption.py

--- plugins/extra/caption.py
+++ plugins/extra/caption.py
@@ -21,19 +21,6 @@ async def get_caption(name):
         newcap=name
     return newcap 
 
-def content(msg: Message) -> [None, str]:
-    text_to_return = msg.text
-
-    if msg.text is None:
-        return None
-    if " " in text_to_return:
-        try:
-            return msg.text.split(None, 1)[1]
-        except IndexError:
-            return None
-    else:
-        return None
-
 @Client.on_message(filters.private & filters.command("caption"))
 async def start(bot, message): 
     caption = content(message)
@@ -42,7 +29,7 @@ async def start(bot, message):
 @Client.on_message(filters.channel & filters.document) #add more filters if you want.
 async def caption(bot, message):
    try:
-       await message.edit(caption(name=await get_caption(message.document.file_name),    
+       await message.edit(CAPTION.format(name=await get_caption(message.document.file_name),    
                                          size=await get_size(message.document.file_size))
                           )
    except Exception as e:
