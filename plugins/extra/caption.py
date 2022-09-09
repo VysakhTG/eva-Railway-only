fa2b6081b4a3b4a7a852742a9db43938a7ee23c1
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-09 14:27:23 +0530

    Create caption.py

---
+++ plugins/extra/caption.py
@@ -0,0 +1,35 @@
+import os
+from pyrogram import Client, filters
+
+async def get_size(size):
+    p=size/1048597           #size in MB
+    if p>1024:
+         c=round(p/1024, 2)
+         filezize=f"{c} GB"
+    elif p>1:
+         c=round(p, 2)
+         filezize=f"{c} MB"
+    else:
+         c=round(size/1024, 1)
+         filezize=f"{c} KB"
+    return filezize
+
+async def get_caption(name):
+    if "_" in name:
+        newcap=name.replace("_", " ")     
+    else:
+        newcap=name
+    return newcap
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start(bot, message):
+    await message.reply(f"Hello {message.from_user.mention},\nI will edit channel message's captions.\nAdd me to your channel with necessary permissions.")
+
+@Client.on_message(filters.channel & filters.document) #add more filters if you want.
+async def caption(bot, message):
+   try:
+       await message.edit(CAPTION.format(name=await get_caption(message.document.file_name),    
+                                         size=await get_size(message.document.file_size))
+                          )
+   except Exception as e:
+       print(e)
