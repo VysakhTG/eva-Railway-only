992ecacc660c7267550fa303edd4f3960af8b594
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-11 19:53:09 +0530

    Chumma oru cheriya test😂

---
+++ plugins/extra/Chumma.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+import datetime
+import time
+from database.users_chats_db import db
+from info import ADMINS
+from utils import broadcast_messages
+import asyncio
+        
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
+    )
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    total_groups = await db.total_groups_count()
+    done = 0
+    blocked = 0
+    deleted = 0
+    failed =0
