c391accabd6150fca30e85ad29d86d1908333372
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-11 20:09:14 +0530

    Update Chumma.py

--- plugins/extra/Chumma.py
+++ plugins/extra/Chumma.py
@@ -6,18 +6,21 @@ from info import ADMINS
 from utils import broadcast_messages
 import asyncio
         
-@Client.on_message(filters.command("broadcast1") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
+start_time = time.time()
     total_users = await db.total_users_count()
     total_groups = await db.total_chat_count()
     done = 0
     blocked = 0
     deleted = 0
     failed =0
+
+
+@Client.on_message(filters.command("broadcast1") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting {total_groups} s...'
+    )
+    
