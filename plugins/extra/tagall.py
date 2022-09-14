c518d5e7924b8b4513fc42b13d07e33d97bacf39
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-14 22:09:53 +0530

    Create tagall.py

---
+++ plugins/extra/tagall.py
@@ -0,0 +1,16 @@
+from pyrogram import filters, Client
+
+@client.on_message(filters.command("tagall")) 
+async def tagall(client, message):
+    await message.reply("`Processing.....`")
+    sh = get_text(message)
+    if not sh:
+        sh = "Hi!"
+    mentions = ""
+    async for member in client.iter_chat_members(message.chat.id):
+        mentions += member.user.mention + " "
+    n = 4096
+    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
+    for i in kk:
+        j = f"<b>{sh}</b> \n{i}"
+        await client.send_message(message.chat.id, j, parse_mode="html")
