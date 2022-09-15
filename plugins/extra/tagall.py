fd58e284e95c1f2b7bb4b39a0757a8a51d798dc8
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-15 19:06:10 +0530

    Update tagall.py

--- plugins/extra/tagall.py
+++ plugins/extra/tagall.py
@@ -1,6 +1,6 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message 
-from plugins.helper_functions.extract_user import admins_only, get_text
+from extrahelp import admins_only, get_text
 def content(msg: Message) -> [None, str]:
     text_to_return = msg.text
 
