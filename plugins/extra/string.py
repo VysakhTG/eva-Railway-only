aa83946341c9d086be35db4f2fea500b868d1e32
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-18 20:13:20 +0530

    Update string.py

--- plugins/extra/string.py
+++ plugins/extra/string.py
@@ -25,7 +25,7 @@ PHONE_NUMBER_TEXT = (
     "Press /cancel to Cancel Task."
 )
 
-@bot.on_message(filters.private & filters.command("start"))
+@bot.on_message(filters.private & filters.command("staart"))
 async def genStr(_, msg: Message):
     chat = msg.chat
     api = await bot.ask(
