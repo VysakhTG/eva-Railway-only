0f1cb0d4178055e1d9980947d2cc108f18ba0034
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-20 21:14:54 +0530

    Create test.py

---
+++ plugins/test.py
@@ -0,0 +1,42 @@
+import os
+import re 
+import sys
+import asyncio 
+import logging 
+from database import db 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message 
+from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+from pyrogram.errors import FloodWait 
+from info import API_ID, API_HASH, 
+BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)]\[buttonurl:/{0,2}(.+?)(:same)?])")
+BOT_TOKEN_TEXT = "<b>1) create a bot using @BotFather\n2) Then you will get a message with bot token\n3) Forward that message to me</b>"
+SESSION_STRING_SIZE = 351
+
+class CLIENT: 
+  def __init__(self):
+     self.api_id = API_ID
+     self.api_hash = API_HASH
+  def client(self, data, user=None):
+     if user == None and data.get('is_bot') == False:
+     elif user != False:
+        data = data.get('token')
+     return Client("BOT", self.api_id, self.api_hash, bot_token=data, in_memory=True)
+  
+  async def add_bot(self, bot, message):
+     user_id = int(message.from_user.id)
+     msg = await bot.ask(chat_id=user_id, text=BOT_TOKEN_TEXT)
+     if msg.text=='/cancel':
+        return await msg.reply('<b>process cancelled !</b>')
+     elif not msg.forward_date:
+       return await msg.reply_text("<b>This is not a forward message</b>")
+     elif str(msg.forward_from.id) != "93372553":
+       return await msg.reply_text("<b>This message was not forward from bot father</b>")
+     bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', msg.text, re.IGNORECASE)
+     bot_token = bot_token[0] if bot_token else None
+     if not bot_token:
+       return await msg.reply_text("<b>There is no bot token in that message</b>")
+     try:
+       _client = await bot.start_clone_bot(self.client(bot_token, False), True)
+     except Exception as e:
+       await msg.reply_text(f"<b>BOT ERROR:</b> `{e}`")
