3e72d66edfe89195c40a3a1baf2ec46b4a3b481a
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-20 21:24:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/VysakhTG/eva.git /eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /eva
 fi
-cd /EvaMaria
+cd /eva
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
