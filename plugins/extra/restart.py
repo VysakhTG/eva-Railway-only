f66638769ae9417d9aafedcc98fb6004b158ea65
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-22 22:11:00 +0530

    Update restart.py

--- plugins/extra/restart.py
+++ plugins/extra/restart.py
@@ -1,6 +1,7 @@
 from pyrogram import filters, Client
 from info import ADMINS
 import sys
+Import asyncio 
 
 @Client.on_message(filters.private & filters.command(['restart']))
 async def restart(client, message):
