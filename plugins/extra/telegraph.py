6a52cd75b2be8a98f8b37655200d581cd883c5cf
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-23 10:48:07 +0530

    Update telegraph.py

--- plugins/extra/telegraph.py
+++ plugins/extra/telegraph.py
@@ -1,95 +1,81 @@
-import os 
-import aiofiles 
+import os
+import requests
 from PIL import Image
-from aiofiles import os 
-from html_telegraph_poster import TelegraphPoster
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from telegraph import upload_file 
 from pyrogram.types import Message
 
-_T_LIMIT = 5242880 
+extensions = ["jpg", "jpeg", "png", "gif", "mp4"]
+size_limit = 5242880
+size_error = "Files with size more than 5 MB are not accepted."
+status_text = "Converting and Uploading..."
 
-@Client.on_message(filters.private & filters.command(["telegraph"]))
-async def telegraph(client, message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply_text("reply to media or text")
-        return
-    if not ((replied.photo and replied.photo.file_size <= _T_LIMIT)
-            or (replied.animation and replied.animation.file_size <= _T_LIMIT)
-            or (replied.video and replied.video.file_name.endswith('.mp4')
-                and replied.video.file_size <= _T_LIMIT)
-            or (replied.sticker and replied.sticker.file_name.endswith('.webp'))
-            or replied.text
-            or (replied.document
-                and replied.document.file_name.endswith(
-                    ('.jpg', '.jpeg', '.png', '.gif', '.mp4', '.html', '.txt', '.py'))
-                and replied.document.file_size <= _T_LIMIT)):
-        await message.reply_text("not supported!")
-        return
-    await message.reply_text("`processing...`")
-    if (replied.text
-        or (replied.document
-            and replied.document.file_name.endswith(
-            ('.html', '.txt', '.py')))):
-        if replied.document:
-            dl_loc = await message.client.download_media(
-                message=message.reply_to_message,
-                file_name=config.Dynamic.DOWN_PATH,
-                progress=progress,
-                progress_args=(message, "trying to download")
-            )
-            async with aiofiles.open(dl_loc, "r") as jv:
-                text = await jv.read()
-            header = message.input_str
-            if not header:
-                header = "Pasted content by @theuserge"
-            await os.remove(dl_loc)
+
+async def work_to_do(message: Message):
+    user_id = message.from_user.id
+    message_id = message.message_id
+    name_format = f"StarkBots_{user_id}_{message_id}"
+    if message.document:
+        extension = message.document.file_name[-3:]
+        if extension not in extensions:
+            await message.reply("This format is not supported by telegraph", quote=True)
+            return
+        elif message.document.file_size > size_limit:
+            await message.reply(size_error, quote=True)
+            return
+        status = await message.reply(status_text, quote=True)
+        file = await message.download(file_name=f"{name_format}.{extension}")
+    elif message.sticker:
+        if not message.sticker.is_animated:
+            status = await message.reply(status_text, quote=True)
+            sticker = await message.download(file_name=f"{name_format}.webp")
+            img = Image.open(sticker).convert('RGB')
+            img.save(f'downloads/{name_format}.jpg', 'jpeg')
+            file = f'downloads/{name_format}.jpg'
+            os.remove(sticker)
         else:
-            content = message.reply_to_message.text.html
-            if "|" in content and not content.startswith("<"):
-                content = content.split("|", maxsplit=1)
-                header = content[0]
-                text = content[1]
-            else:
-                text = content
-                header = "Pasted content by @theuserge"
-        t_url = await pool.run_in_thread(post_to_telegraph)(header, text.replace("\n", "<br>"))
-        jv_text = f"**[Here Your Telegra.ph Link!]({t_url})**"
-        await message.edit(text=jv_text, disable_web_page_preview=True)
-        return
-    dl_loc = await message.client.download_media(
-        message=message.reply_to_message,
-        file_name=config.Dynamic.DOWN_PATH,
-        progress=progress,
-        progress_args=(message, "trying to download")
-    )
-    if replied.sticker:
-        img = Image.open(dl_loc).convert('RGB')
-        img.save(f'{config.Dynamic.DOWN_PATH}/userge.png', 'png')
-        await os.remove(dl_loc)
-        dl_loc = f'{config.Dynamic.DOWN_PATH}/userge.png'
-    await message.edit("`uploading to telegraph...`")
-    try:
-        response = await pool.run_in_thread(upload_file)(dl_loc)
-    except Exception as t_e:
-        await message.err(str(t_e))
+            await message.reply("Animated Stickers are not supported yet.", quote=True)
+            return
+    elif message.photo:
+        if message.photo.file_size > size_limit:
+            await message.reply(size_error, quote=True)
+            return
+        status = await message.reply(status_text, quote=True)
+        file = await message.download(file_name=f"{name_format}.jpg")
+    elif message.video:
+        if message.video.file_size > size_limit:
+            await message.reply(size_error, quote=True)
+            return
+        status = await message.reply(status_text, quote=True)
+        file = await message.download(file_name=f"{name_format}.mp4")
+    elif message.video_note:
+        if message.video_note.file_size > size_limit:
+            await message.reply(size_error, quote=True)
+            return
+        status = await message.reply(status_text, quote=True)
+        file = await message.download(file_name=f"{name_format}.mp4")
+    elif message.animation:
+        if message.animation.file_size > size_limit:
+            await message.reply(size_error, quote=True)
+            return
+        status = await message.reply(status_text, quote=True)
+        file = await message.download(file_name=f"{name_format}.gif")
     else:
-        await message.edit(f"**[Here Your Telegra.ph Link!](https://telegra.ph{response[0]})**")
-    finally:
-        await os.remove(dl_loc)
+        await message.reply("This is not one of the supported types.", quote=True)
+        return
+    url = await upload(file)
+    os.remove(file)
+    return url, status
 
 
-def post_to_telegraph(a_title: str, content: str) -> str:
-    """ Create a Telegram Post using HTML Content """
-    post_client = TelegraphPoster(use_api=True)
-    auth_name = "@TheUserge"
-    post_client.create_api_token(auth_name)
-    post_page = post_client.post(
-        title=a_title,
-        author=auth_name,
-        author_url="https://telegram.me/theUserge",
-        text=content
-    )
-    return post_page['url']
+async def upload(file):
+    files = {'files': open(file, 'rb')}
+    r = requests.post("https://telegra.ph/upload", files=files)
+    response = r.json()
+    if isinstance(response, list):
+        error = response[0].get('error')
+    else:
+        error = response.get('error')
+    if error:
+        url = error
+    else:
+        url = "https://telegra.ph" + response[0].get("src")
+    return url
