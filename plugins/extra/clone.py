76ad7f1c5b0dc6e2cc0639e8ca099fc2c5f9bcba
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-28 15:51:05 +0530

    Update clone.py

--- plugins/extra/clone.py
+++ plugins/extra/clone.py
@@ -34,3 +34,14 @@ async def add_clone(bot, message):
       _client = await bot.sign_in_bot(bot.bot_token) 
     except Exception as e:
       await message.reply_text(f"<b>BOT ERROR:</b> `{e}`")
+    _bot = _client.me
+     details = {
+       'id': _bot.id,
+       'is_bot': True,
+       'user_id': user_id,
+       'name': _bot.first_name,
+       'token': bot_token,
+       'username': _bot.username 
+     }
+     await db.add_bot(details)
+     return True
