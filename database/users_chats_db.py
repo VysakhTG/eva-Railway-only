d69b034e08e4655ce7c605fdaf221af68bea91f7
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-28 16:32:48 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -93,9 +93,23 @@ class Database:
 
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
-        await self.grp.insert_one(chat)
-    
+        await self.grp.insert_one(chat) 
 
+    async def add_bot(self, datas):
+       if not await self.is_bot_exist(datas['user_id']):
+          await self.bot.insert_one(datas)
+    
+    async def remove_bot(self, user_id):
+       await self.bot.delete_many({'user_id': int(user_id)})
+      
+    async def get_bot(self, user_id: int):
+       bot = await self.bot.find_one({'user_id': user_id})
+       return bot if bot else None
+                                          
+    async def is_bot_exist(self, user_id):
+       bot = await self.bot.find_one({'user_id': user_id})
+       return bool(bot)
+    
     async def get_chat(self, chat):
         chat = await self.grp.find_one({'id':int(chat)})
         return False if not chat else chat.get('chat_status')
