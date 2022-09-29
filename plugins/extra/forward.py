22e4c64b784aa959fd2d495f2b131ac3fa56c85c
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-29 20:04:59 +0530

    Update forward.py

--- plugins/extra/forward.py
+++ plugins/extra/forward.py
@@ -5,7 +5,7 @@ import time
 import os 
 from info import ADMINS 
 
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
+@Client.on_message(filters.command("forward") & filters.user(ADMINS))
 async def forward(bot, message): 
     to_channel = 
     from_channel =
@@ -13,11 +13,11 @@ async def forward(bot, message):
     end_skip =
     await message.reply_text('**Forwarding Started**')
     try:
-        for i in range(s_msg, f_msg):
+        for i in range(start_skip, end_skip):
             try:
                 await bot.copy_message(
-                    chat_id= t_chat,
-                    from_chat_id= i_chat,
+                    chat_id= to_channel,
+                    from_chat_id= from_channel,
                     message_id= i
                 )
                 time.sleep(2)
