c757c13319b190d2dc47d44770011fb7792f53a7
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-09-30 19:15:02 +0530

    Update forward.py

--- plugins/extra/forward.py
+++ plugins/extra/forward.py
@@ -5,24 +5,26 @@ import time
 import os 
 from info import ADMINS 
 
+TO_CHANNEL = -1737494519
+FROM_CHANNEL = -1752475005
+START_M = 4
+END_M = 2000
+ 
 @Client.on_message(filters.command("forward") & filters.user(ADMINS))
 async def forward(bot, message): 
-    to_channel = "-1737494519"
-    from_channel = "-1752475005"
-    start_skip = "1"
-    end_skip = "516348"
-    await message.reply_text('**Forwarding Started**')
+    await message.reply_text('**Forwarding Started**\n\nPress /restart to Stop and /log to get log TXT file')
     try:
-        for i in range(start_skip, end_skip):
+        for i in range(START_M, END_M):
             try:
                 await bot.copy_message(
-                    chat_id= to_channel,
-                    from_chat_id= from_channel,
-                    message_id= i
+                    chat_id= TO_CHANNEL,
+                    from_chat_id= FROM_CHANNEL,
+                    message_id= i,
+                    caption=Config.CAPTION
                 )
                 time.sleep(2)
             except Exception:
                 continue
     except Exception as e:
-        await message.reply_text(str(e))
-    await message.reply_text("Done Forwarding")
+        await m.reply_text(str(e))
+    await m.reply_text("Done Forwarding")
