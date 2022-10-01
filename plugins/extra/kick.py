18ed25852442dec283e2cc5603b10f1f7d166909
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-01 18:35:59 +0530

    Update kick.py

--- plugins/extra/kick.py
+++ plugins/extra/kick.py
@@ -41,3 +41,8 @@ def NewChat(bot,message):
     for i in a:
         try:
             bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
