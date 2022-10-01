24cbb17ffc39d69c89d7b484cfd01ae440d8735a
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-01 21:14:22 +0530

    Update approve.py

--- plugins/extra/approve.py
+++ plugins/extra/approve.py
@@ -1,12 +1,7 @@
 from pyrogram import Client, filters 
 from pyrogram.types import Message, User, ChatJoinRequest
-
-TEXT =  "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved"
-
-
 @Client.on_chat_join_request((filters.group | filters.channel))
 async def autoapprove(bot: Client, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
     await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    await bot.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
