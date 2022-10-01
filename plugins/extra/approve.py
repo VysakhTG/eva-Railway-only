194f806b88e28b3bc90491169ca785da3b976835
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-01 19:36:33 +0530

    Update approve.py

--- plugins/extra/approve.py
+++ plugins/extra/approve.py
@@ -8,5 +8,5 @@ TEXT =  "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved"
 async def autoapprove(bot: Client, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+    await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    await bot.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
