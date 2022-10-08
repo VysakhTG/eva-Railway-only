bc1774c7b9b27aa88d52031fd5e06433619e2f18
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-08 15:06:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -137,7 +137,7 @@ async def start(client, message):
                 f_caption = f"{title}"
             try:
                 await client.send_cached_media(
-                    chat_id=message.from_user.id,
+                    chat_id=LOG_CHANNEL,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
