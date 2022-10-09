532b5d908c4e7d93026e52551283cccb4d67fe1f
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-09 20:51:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,6 +41,7 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_URL = environ.get('DATABASE_URL')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
