4020f1108a30422356e50860e3d430c33bb43aba
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-09 14:36:58 +0530

    Update rename.py

--- plugins/extra/rename.py
+++ plugins/extra/rename.py
@@ -57,55 +57,120 @@ def TimeFormatter(milliseconds: int) -> str:
           ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2] 
 
-@Client.on_message(filters.private & filters.command("rename"))             
-async def rename_file(bot, msg):
-    reply = msg.reply_to_message
-    if len(msg.command) < 2 or not reply:
-       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
-    media = reply.document or reply.audio or reply.video
-    if not media:
-       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
-    og_media = getattr(reply, reply.media.value)
-    new_name = msg.text.split(" ", 1)[1]
-    sts = await msg.reply_text("Renaming.....")
-    c_time = time.time()
-    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
-    filesize = humanbytes(og_media.file_size)                
-    if CAPTION:
-        cap = CAPTION.format(file_name=new_name, file_size=filesize)
-    else:
-        cap = f"{new_name}\n\n💽 size : {filesize}"
-    raw_thumbnail = temp.THUMBNAIL 
-    if raw_thumbnail:
-        og_thumbnail = await bot.download_media(raw_thumbnail)
-    else:
-        og_thumbnail = await bot.download_media(og_media.thumbs[0].file_id)
-    await sts.edit("Trying to Uploading")
-    c_time = time.time()
-    await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))                                    
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+async def rename_start(client, message):
+    file = getattr(message, message.media.value)
+    filename = file.file_name
+    filesize = humanize.naturalsize(file.file_size) 
+    fileid = file.file_id
     try:
-        os.remove(downloaded)
-        os.remove(og_thumbnail)
+        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
+        buttons = [[ InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename") ],
+                   [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
+        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+        await sleep(FLOOD)
+    except FloodWait as e:
+        await sleep(e.value)
+        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
+        buttons = [[ InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename") ],
+                   [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
+        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
     except:
         pass
-    await sts.delete()
-@Client.on_message(filters.private & filters.command("set"))                            
-async def set_tumb(bot, msg):
-    replied = msg.reply_to_message
-    if not replied:
-        await msg.reply("use this command with Reply to a photo")
-        return
-    if not msg.reply_to_message.photo:
-       await msg.reply("Oops !! this is Not a photo")
-       return
-    Tumb = msg.reply_to_message.photo.file_id
-    temp.THUMBNAIL = Tumb
-    return await msg.reply(f"Temporary Thumbnail saved✅️ \nDo You want permanent thumbnail. \n\n`{Tumb}` \n\n👆👆 please add this id to your server enviro with key=`THUMBNAIL`")            
+@Client.on_callback_query(filters.regex('cancel'))
+async def cancel(bot,update):
+	try:
+           await update.message.delete()
+	except:
+           return
 
-@Client.on_message(filters.private & filters.command("view"))                            
-async def del_tumb(bot, msg):
-    if temp.THUMBNAIL:
-        await msg.reply_photo(photo=temp.THUMBNAIL, caption="this is your current thumbnail")
-    else:
-        await msg.reply_text(text="you don't have any thumbnail")
-
+@Client.on_callback_query(filters.regex('rename'))
+async def rename(bot,update):
+	user_id = update.message.chat.id
+	date = update.message.date
+	await update.message.delete()
+	await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
+	reply_to_message_id=update.message.reply_to_message.id,  
+	reply_markup=ForceReply(True))
+	
+@Client.on_callback_query(filters.regex("upload"))
+async def doc(bot,update):
+     type = update.data.split("_")[1]
+     new_name = update.message.text
+     new_filename = new_name.split(":-")[1]
+     file_path = f"downloads/{new_filename}"
+     file = update.message.reply_to_message
+     ms = await update.message.edit("𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳...")
+     c_time = time.time()
+     try:
+     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳....",  ms, c_time   ))
+     except Exception as e:
+     	await ms.edit(e)
+     	return 
+     splitpath = path.split("/downloads/")
+     dow_file_name = splitpath[1]
+     old_file_name =f"downloads/{dow_file_name}"
+     os.rename(old_file_name,file_path)
+     duration = 0
+     try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+     except:
+        pass
+     user_id = int(update.message.chat.id) 
+     ph_path = None 
+     media = getattr(file, file.media.value)
+     c_caption = await db.get_caption(update.message.chat.id)
+     c_thumb = await db.get_thumbnail(update.message.chat.id)
+     if c_caption:
+         caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=convert(duration))
+     else:
+         caption = f"**{new_filename}**"
+     if (media.thumbs or c_thumb):
+         if c_thumb:
+            ph_path = await bot.download_media(c_thumb) 
+         else:
+            ph_path = await bot.download_media(media.thumbs[0].file_id)
+         Image.open(ph_path).convert("RGB").save(ph_path)
+         img = Image.open(ph_path)
+         img.resize((320, 320))
+         img.save(ph_path, "JPEG")
+     await ms.edit("𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....")
+     c_time = time.time() 
+     try:
+        if type == "document":
+           await bot.send_document(
+		    update.message.chat.id,
+                    document=file_path,
+                    thumb=ph_path, 
+                    caption=caption, 
+                    progress=progress_for_pyrogram,
+                    progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....",  ms, c_time   ))
+        elif type == "video": 
+            await bot.send_video(
+		    update.message.chat.id,
+		    video=file_path,
+		    caption=caption,
+		    thumb=ph_path,
+		    duration=duration,
+		    progress=progress_for_pyrogram,
+		    progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....",  ms, c_time))
+        elif type == "audio": 
+            await bot.send_audio(
+		    update.message.chat.id,
+		    audio=file_path,
+		    caption=caption,
+		    thumb=ph_path,
+		    duration=duration,
+		    progress=progress_for_pyrogram,
+		    progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....",  ms, c_time   )) 
+     except Exception as e: 
+         await ms.edit(e) 
+         os.remove(file_path)
+         if ph_path:
+           os.remove(ph_path)
+     await ms.delete() 
+     os.remove(file_path) 
+     if ph_path:
+        os.remove(ph_path) 
