3a22bf4499d5b6a94b00fc70a118624447e747a5
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-09 17:57:58 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -43,7 +43,20 @@ class Database:
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
+   
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('file_id', None) 
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
     
+    async def get_caption(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('caption', None)
+
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
