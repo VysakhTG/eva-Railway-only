f78bed91f831ffcca94a4ef9af04667bd9c6d037
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-13 05:00:12 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -24,20 +24,5 @@ class CLIENT:
      return Client("BOT", self.api_id, self.api_hash, bot_token=data, in_memory=True)
 
 @Client.on_message((filters.forwarded & filters.user(93372553))
-  async def add_bot(self, bot, message):
-     user_id = int(message.from_user.id)
-     msg = await bot.ask(chat_id=user_id, text=BOT_TOKEN_TEXT)
-     if msg.text=='/cancel':
-        return await msg.reply('<b>process cancelled !</b>')
-     elif not msg.forward_date:
-       return await msg.reply_text("<b>This is not a forward message</b>")
-     elif str(msg.forward_from.id) != "93372553":
-       return await msg.reply_text("<b>This message was not forward from bot father</b>")
-     bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', msg.text, re.IGNORECASE)
-     bot_token = bot_token[0] if bot_token else None
-     if not bot_token:
-       return await msg.reply_text("<b>There is no bot token in that message</b>")
-     try:
-       _client = await bot.start_clone_bot(self.client(bot_token, False), True)
-     except Exception as e:
-       await msg.reply_text(f"<b>BOT ERROR:</b> `{e}`")
+async def add_bot(self, bot, message):
+    await message.reply("**process cancelled !**")
