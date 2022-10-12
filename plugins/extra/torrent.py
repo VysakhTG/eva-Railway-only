5831cd1ae34a17e40a718dae3ba6285b31f2575d
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-12 19:30:19 +0530

    Update torrent.py

--- plugins/extra/torrent.py
+++ plugins/extra/torrent.py
@@ -1,135 +1,30 @@
 import os
 import aiohttp
-import json
-from pyrogram import Client, filters, emoji
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from requests.utils import requote_uri
+
+API_1337x = "https://api.abir-hasan.tk/1337x?query={}&limit={}"
+API_YTS = "https://api.abir-hasan.tk/yts?query={}&limit={}"
+API_PIRATEBAY = "https://api.abir-hasan.tk/piratebay?query={}&limit={}"
+API_ANIME = "https://api.abir-hasan.tk/anime?query={}&limit={}"
+MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
+
+async def Search1337x(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_1337x.format(query, MAX_INLINE_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else [] 
+
+async def SearchYTS(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_YTS.format(query, MAX_INLINE_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+async def SearchPirateBay(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_PIRATEBAY.format(query, MAX_INLINE_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
 
 
-m = None
-i = 0
-a = None
-query = None
-
-
-@Client.on_message(filters.command(["torrent"]))
-async def torrent(_, message):
-    global m
-    global i
-    global a
-    global query
-    try:
-        await message.delete()
-    except:
-        pass
-    if len(message.command) < 2:
-        await message.reply_text("/torrent ᴀɴᴅ ᴛʏᴘᴇ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ")
-        return
-    query = message.text.split(None, 1)[1].replace(" ", "%20")
-    m = await message.reply_text("Searching\nThis might take a while")
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://itor.api-zero.workers.dev/?name={query}") \
-                    as resp:
-                a = json.loads(await resp.text())
-    except:
-        await m.edit("Found Nothing.")
-        return
-    result = (
-        f"**Page - {i+1}**\n\n"
-        f"➲Name: {a[i]['name']}\n"
-        f"➲Link: `{a[i]['link']}`\n\n\n"
-    )
-    await m.edit(
-        result,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(f"Next",
-                                         callback_data="next"),
-                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
-                                         callback_data="delete")
-                ]
-            ]
-        ),
-        parse_mode="markdown",
-    )
-
-
-@Client.on_callback_query(filters.regex("next"))
-async def callback_query_next(_, message):
-    global i
-    global m
-    global a
-    global query
-    i += 1
-    result = (
-        f"**Page - {i+1}**\n\n"
-        f"➲Name: {a[i]['name']}\n"
-        f"➲Link: `{a[i]['link']}`\n\n\n"
-    )
-    await m.edit(
-        result,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(f"Back",
-                                         callback_data="back"),
-                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
-                                         callback_data="delete"),
-                    InlineKeyboardButton(f"Next",
-                                         callback_data="next")
-                    
-                ]
-            ]
-        ),
-        parse_mode="markdown",
-    )
-
-
-@Client.on_callback_query(filters.regex("back"))
-async def callback_query_previous(_, message):
-    global i
-    global m
-    global a
-    global query
-    i -= 1
-    result = (
-        f"**Page - {i+1}**\n\n"
-        f"➲Name: {a[i]['name']}\n"
-        f"➲Link: `{a[i]['link']}`\n\n\n"
-    )
-    await m.edit(
-        result,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(f"Back",
-                                         callback_data="back"),
-                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
-                                         callback_data="delete"),
-                    InlineKeyboardButton(f"Next",
-                                         callback_data="next")
-                ]
-            ]
-        ),
-        parse_mode="markdown",
-    )
-
-
-@Client.on_callback_query(filters.regex("delete"))
-async def callback_query_delete(_, message):
-    global m
-    global i
-    global a
-    global query
-    await m.delete()
-    m = None
-    i = 0
-    a = None
-    query = None
-
-__help__ = """
- - /torrent <movie name>: Get Your Torrent Link From Various Resource. 
-"""
-
-__mod_name__ = "Torrent"
+async def SearchAnime(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_ANIME.format(query, MAX_INLINE_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
