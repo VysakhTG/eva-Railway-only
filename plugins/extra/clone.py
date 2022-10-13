8369472eb1457e6781e4fd4e6c7cf01d57a43980
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-13 06:20:34 +0530

    Update clone.py

--- plugins/extra/clone.py
+++ plugins/extra/clone.py
@@ -29,6 +29,6 @@ async def add_bot(self, message):
     if str(message.forward_from.id) != "93372553":
       return await message.reply_text("<b>process cancelled !</b>")
     bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', message.text, re.IGNORECASE)
-    bot_token = bot_token[0] if bot_token else None
-    if not bot_token:
+    match = bot_token[0] if bot_token else None
+    if not match:
        return await message.reply_text("<b>There is no bot token in that message</b>")
